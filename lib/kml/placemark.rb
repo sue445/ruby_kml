5cb9a75146e051ffbf15b771d8c02a32a7188d31
sue445 <sue445@sue445.net>
2016-06-08 21:54:46 +0900

    Support style in placemark

--- lib/kml/placemark.rb
+++ lib/kml/placemark.rb
@@ -17,7 +17,7 @@
 #     <StyleSelector>...</StyleSelector>
 #     <Region>...</Region>
 #     <Metadata>...</Metadata>
-#   
+#
 #     <!-- specific to Placemark element -->
 #     <Geometry>...</Geometry>
 #   </Placemark>
@@ -25,13 +25,15 @@
 module KML
   class Placemark < KML::Container
     attr_accessor :geometry
-    
+    attr_accessor :style
+
     def render(xm=Builder::XmlMarkup.new(:indent => 2))
       xm.Placemark {
         super
         features.each { |f| f.render(xm) }
         plain_children.each { |c| xm << c }
         geometry.render(xm) unless geometry.nil?
+        style.render(xm) unless style.nil?
       }
     end
   end
